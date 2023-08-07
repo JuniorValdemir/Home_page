c7cd189d67908f7b441398042056bc4011a524eb
JVNJ1 <juniorvaldemir87@gmail.com>
2023-08-07 10:28:18 -0300

    All change.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function btnLogin() {
+    alert("Sistema em Desenvolvimento!");
+}
+
+function rememberAcc() {
+    alert("Sistema em Desenvolvimento!");
+}
+
+function registerAcc() {
+    alert("Sistema em Desenvolvimento!");
+}
